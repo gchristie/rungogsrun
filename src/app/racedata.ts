09e29b31c51c386c0839f4fa8d602e6cb9c7a870
gchristie <gchristie@group1auto.com>
2021-02-02 10:53:41 -0600

    1 file modified

--- src/app/racedata.ts
+++ src/app/racedata.ts
@@ -1,68 +1,62 @@
 export var racedata = 
 [ 
 //Upcoming Races
-
-   { name:'Chicago Marathon',PR:'3:24:59',count:1,racetype:'Upcoming Race',comment:'10/10/2021'  }  
-   , { name:'Boston Marathon',PR:'Finish',count:1,racetype:'Upcoming Race',comment:'10/11/2021'  }  
+  { name:'Brazos Bend 50K',PR:'3:24:59',count:1,racetype:'Upcoming Race',comment:'10/10/2021'  }  
+ , { name:'No Label Triathlon',PR:'',count:1,racetype:'Upcoming Race',comment:''  }  
+ , { name:'Chicago Marathon',PR:'3:24:59',count:1,racetype:'Upcoming Race',comment:'10/10/2021'  }  
+ , { name:'Boston Marathon',PR:'Finish',count:1,racetype:'Upcoming Race',comment:'10/11/2021'  }  
 //Ultra Marathons
  , { name:'Rocky Raccoon 50 Mile',PR:'7:41:39',count:1,racetype:'Ultra Marathon',comment:'3rd Place Male 2013'  }  
-, { name:'Snowdrop 100 Miles',PR:'',count:6,racetype:'Ultra Marathon',comment:'3rd Place Male 2019'  }  
-, { name:'Brazos Bend 100 Miles',PR:'',count:1,racetype:'Ultra Marathon',comment:''  }  
-, { name:'Habanero Hunderd 50K',PR:'',count:1,racetype:'Ultra Marathon',comment:''  }  
-, { name:'Habanero Hunderd 100K',PR:'',count:1,racetype:'Ultra Marathon',comment:''  }   
-//Marathons
+ , { name:'Snowdrop 100 Miles',PR:'',count:6,racetype:'Ultra Marathon',comment:'3rd Place Male 2019'  }  
+ , { name:'Brazos Bend 100 Miles',PR:'',count:1,racetype:'Ultra Marathon',comment:''  }  
+ , { name:'Habanero Hunderd 50K',PR:'',count:1,racetype:'Ultra Marathon',comment:''  }  
+ , { name:'Habanero Hunderd 100K',PR:'',count:1,racetype:'Ultra Marathon',comment:''  }   
+ //Marathons
  , { name:'Houston',PR:'2:51:11',count:11,racetype:'Marathon',comment:'Marathon PR'  }
-  , { name:'Twin Cities',PR:'2:52:21',count :3,racetype:'Marathon',comment:'All 3 times under 3 hours' }
-   , { name:'Moray',PR:'2:53:06',count :5,racetype:'Marathon',comment:''  }
-   , { name:'Boston',PR:'2:58:40',count:9,racetype:'Marathon',comment:''  }
+ , { name:'Twin Cities',PR:'2:52:21',count :3,racetype:'Marathon',comment:'All 3 times under 3 hours' }
+ , { name:'Moray',PR:'2:53:06',count :5,racetype:'Marathon',comment:''  }
+ , { name:'Boston',PR:'2:58:40',count:9,racetype:'Marathon',comment:''  }
  , { name:'The Woodlands',PR:'2:58:43',count :4,racetype:'Marathon',comment:'' }  
  , { name:'BCS',PR:'3:04:14',count :3,racetype:'Marathon',comment:'' }
-  , { name:'Seabrook',PR:'3:08:50',count :5,racetype:'Marathon',comment:'2012 1st Place'  }
+ , { name:'Seabrook',PR:'3:08:50',count :5,racetype:'Marathon',comment:'2012 1st Place'  }
  , { name:'RNR Las Vegas',PR:'3:13:05',count :3,racetype:'Marathon',comment:'' }
  , { name:'Tokyo',PR:'3:14:15',count :1,racetype:'Marathon',comment:'' }
  , { name:'RNR San Antonio',PR:'3:23:35',count :1,racetype:'Marathon',comment:'' }
  , { name:'Surfside Beach',PR:'3:43:18',count :1,racetype:'Marathon',comment:'' }
  , { name:'The Army',PR:'5:30:44',count :1,racetype:'Marathon',comment:'' }
  , { name:'Marine Corps',PR:'5:58:00',count :1,racetype:'Marathon',comment:'' } 
-
-  //Virtual Marathons
-   , { name:'Boston',PR:'',count:1,racetype:'Virtual Marathon',comment:''  }
-   , { name:'London',PR:'',count:1,racetype:'Virtual Marathon',comment:''  }
-   , { name:'Marine Corps 50K',PR:'',count:1,racetype:'Virtual Marathon',comment:''  }
-   , { name:'Marine Corps',PR:'',count:1,racetype:'Virtual Marathon',comment:''  }
-   , { name:'Houston',PR:'',count:1,racetype:'Virtual Marathon',comment:''  }
-  //Half Marathons
-   , { name:'Houston',PR:'	1:20:56',count:7,racetype:'Half Marathon',comment:'Half Marathon PR' } 
-   , { name:'RNR San Antonio',PR:'1:24:08',count:4,racetype:'Half Marathon',comment:'' } 
-    , { name:'Katy',PR:'1:24:11',count:2,racetype:'Half Marathon',comment:'' }
-  , { name:'Peterhead',PR:'1:24:21',count:1,racetype:'Half Marathon',comment:'' } 
-  , { name:'Angies Half Crazy',PR:'1:24:38',count:5,racetype:'Half Marathon',comment:'' } 
-  , { name:'Rhythm and Blues',PR:'1:24:40',count:1,racetype:'Half Marathon',comment:'' } 
+ //Virtual Marathons
+ , { name:'Boston',PR:'',count:1,racetype:'Virtual Marathon',comment:''  }
+ , { name:'London',PR:'',count:1,racetype:'Virtual Marathon',comment:''  }
+ , { name:'Marine Corps 50K',PR:'',count:1,racetype:'Virtual Marathon',comment:''  }
+ , { name:'Marine Corps',PR:'',count:1,racetype:'Virtual Marathon',comment:''  }
+ , { name:'Houston',PR:'',count:1,racetype:'Virtual Marathon',comment:''  }
+ //Half Marathons
+ , { name:'Houston',PR:'	1:20:56',count:7,racetype:'Half Marathon',comment:'Half Marathon PR' } 
+ , { name:'RNR San Antonio',PR:'1:24:08',count:4,racetype:'Half Marathon',comment:'' } 
+ , { name:'Katy',PR:'1:24:11',count:2,racetype:'Half Marathon',comment:'' }
+ , { name:'Peterhead',PR:'1:24:21',count:1,racetype:'Half Marathon',comment:'' } 
+ , { name:'Angies Half Crazy',PR:'1:24:38',count:5,racetype:'Half Marathon',comment:'' } 
+ , { name:'Rhythm and Blues',PR:'1:24:40',count:1,racetype:'Half Marathon',comment:'' } 
  , { name:'BCS',PR:'1:24:43',count:2,racetype:'Half Marathon',comment:'' } 
  , { name:'Because we care',PR:'1:25:09',count:1,racetype:'Half Marathon',comment:'1st Place' } 
-  , { name:'Texas',PR:'1:26:44',count:2,racetype:'Half Marathon',comment:'' } 
+ , { name:'Texas',PR:'1:26:44',count:2,racetype:'Half Marathon',comment:'' } 
  , { name:'Clear Lake City',PR:'1:26:59',count:1,racetype:'Half Marathon',comment:'1st Place' } 
  , { name:'Shiner',PR:'1:30:00',count:1,racetype:'Half Marathon',comment:''  } 
  , { name:'Vintage Park',PR:'1:30:16',count:1,racetype:'Half Marathon',comment:''  } 
-  , {  name:'The Woodlands',PR:'1:31:29',count:1,racetype:'Half Marathon',comment:'' } 
-
-   , { name:'La Porte',PR:'1:32:53',count:1,racetype:'Half Marathon',comment:'' } 
-    , { name:'Biggest Loser',PR:'1:32:58',count:1,racetype:'Half Marathon',comment:'' } 
+ , {  name:'The Woodlands',PR:'1:31:29',count:1,racetype:'Half Marathon',comment:'' } 
+ , { name:'La Porte',PR:'1:32:53',count:1,racetype:'Half Marathon',comment:'' } 
+ , { name:'Biggest Loser',PR:'1:32:58',count:1,racetype:'Half Marathon',comment:'' } 
  , { name:'RNR Dallas',PR:'1:33:18',count:1,racetype:'Half Marathon',comment:''  } 
-,  { name:'Surfside',PR:'1:34:38',count:1,racetype:'Half Marathon',comment:''  } 
+ ,  { name:'Surfside',PR:'1:34:38',count:1,racetype:'Half Marathon',comment:''  } 
  , { name:'Santa Hustle',PR:'1:34:55',count:1,racetype:'Half Marathon',comment:'' } 
-  , { name:'Love the Half',PR:'1:36:16',count:1,racetype:'Half Marathon',comment:'' } 
-  , { name:'Moray',PR:'1:37:51',count:1,racetype:'Half Marathon',comment:'' } 
-  , { name:'Seabrook Lucky Trail',PR:'1:38:25',count:1,racetype:'Half Marathon',comment:'' } 
+ , { name:'Love the Half',PR:'1:36:16',count:1,racetype:'Half Marathon',comment:'' } 
+ , { name:'Moray',PR:'1:37:51',count:1,racetype:'Half Marathon',comment:'' } 
+ , { name:'Seabrook Lucky Trail',PR:'1:38:25',count:1,racetype:'Half Marathon',comment:'' } 
  , { name:'Sugar & Spice',PR:'1:41:11',count:1,racetype:'Half Marathon',comment:'Catherine finished in 1st place' } 
-  , { name:'Aramco Houston',PR:'1:46:56',count:1,racetype:'Half Marathon',comment:'1st Half Marathon' } 
-   , { name:'Dusk to Dawn',PR:'1:50:50',count:1,racetype:'Half Marathon',comment:'ran woodlands Marathon in morning' } 
-    , { name:'Disney Princess',PR:'1:51:42',count:1,racetype:'Half Marathon',comment:'with Catherine' } 
-   , { name:'RNR Las Vegas',PR:'2:48:22',count:1,racetype:'Half Marathon',comment:''  } 
-   , { name:'Detroit Free Press',PR:'2:52:44',count:1,racetype:'Half Marathon',comment:'after running 87 miles' } 
+ , { name:'Aramco Houston',PR:'1:46:56',count:1,racetype:'Half Marathon',comment:'1st Half Marathon' } 
+ , { name:'Dusk to Dawn',PR:'1:50:50',count:1,racetype:'Half Marathon',comment:'ran woodlands Marathon in morning' } 
+ , { name:'Disney Princess',PR:'1:51:42',count:1,racetype:'Half Marathon',comment:'with Catherine' } 
+ , { name:'RNR Las Vegas',PR:'2:48:22',count:1,racetype:'Half Marathon',comment:''  } 
+ , { name:'Detroit Free Press',PR:'2:52:44',count:1,racetype:'Half Marathon',comment:'after running 87 miles' } 
 ];
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at http://angular.io/license
-*/
