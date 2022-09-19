448c2d4cac4591bf35c30491bae6b72d8c8c8d51
gchristie <gogs@gogs.us>
2022-09-19 13:51:45 -0500

    1 file modified

--- src/app/racedata.ts
+++ src/app/racedata.ts
@@ -12,10 +12,10 @@ export var racedata =
  , { name:'Habanero Hunderd 50K',PR:'',count:1,racetype:'UltraMarathon',comment:''  }  
  , { name:'Habanero Hunderd 100K',PR:'',count:1,racetype:'UltraMarathon',comment:''  }   
  //Marathons
- , { name:'Houston [1 Virtual]',PR:'2:51:11',count:12,racetype:'Marathon',comment:'Marathon PR'  }
+ , { name:'Houston [+ 1 Virtual]',PR:'2:51:11',count:12,racetype:'Marathon',comment:'Marathon PR'  }
  , { name:'Twin Cities',PR:'2:52:21',count :3,racetype:'Marathon',comment:'All 3 times under 3 hours' }
  , { name:'Moray',PR:'2:53:06',count :5,racetype:'Marathon',comment:''  }
- , { name:'Boston  [1 Virtual]',PR:'2:58:40',count:10,racetype:'Marathon',comment:''  }
+ , { name:'Boston  [+ 1 Virtual]',PR:'2:58:40',count:10,racetype:'Marathon',comment:''  }
  , { name:'The Woodlands',PR:'2:58:43',count :4,racetype:'Marathon',comment:'' }  
  , { name:'BCS',PR:'3:04:14',count :3,racetype:'Marathon',comment:'' }
  , { name:'Seabrook',PR:'3:08:50',count :5,racetype:'Marathon',comment:'2012 1st Place'  }
