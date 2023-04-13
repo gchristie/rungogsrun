1dc700b22f2ee8b0dc0e955642144209ea9491f4
gchristie <gogs@gogs.us>
2023-04-13 13:27:22 -0500

    1 file modified

--- src/app/racedata.ts
+++ src/app/racedata.ts
@@ -3,7 +3,7 @@ export var racedata =
 //Upcoming Races
 
  
-  { name:'GVRAT 100KK',PR:'',count:1,racetype:'UpcomingRace',comment:'05/01/2023'  } 
+  { name:'GVRAT Tennessee 1000 Km',PR:'',count:1,racetype:'UpcomingRace',comment:'05/01/2023'  } 
   ,  { name:'Marine Corps 50K',PR:'',count:1,racetype:'UpcomingRace',comment:'10/29/2023'  } 
   ,   { name:'Snowdrop 100 Miles',PR:'',count:1,racetype:'UpcomingRace',comment:'12/30/2023'  }   
 //Ultra Marathons
